e06c52e5c4f312fed583fb3f3b10da338f50c671
LeaderQiu <316894551@qq.com>
2015-05-02 09:12:42 +0800

    1.0

---
+++ XMPP/XMPP/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  XMPP
+//
+//  Created by Kevin on 15/4/12.
+//  Copyright (c) 2015年 Kevin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
